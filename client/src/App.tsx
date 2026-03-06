303166f6b110b41d623f72e5edd7368dabb49fc7
Deep Goyani <177735104+DeepGoyani@users.noreply.github.com>
2026-03-07 00:30:30 +0530

    Update App.tsx with header comments - Commit 52 of 55

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,6 @@
+// Main Application Component
+// Getway Web Application with animated transitions
+
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
